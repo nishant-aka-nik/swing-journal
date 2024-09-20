b4531701daa6afb5b5dff9e83631b6df4136fb21
Nishant Kumar <johndoe@email.com>
2024-09-20 20:51:25 +0530

    new login design

--- src/pages/loginPage.js
+++ src/pages/loginPage.js
@@ -2,8 +2,9 @@ import { createClient } from '@supabase/supabase-js'
 import { useNavigate } from 'react-router-dom';
 import { Auth } from "@supabase/auth-ui-react";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
-import React, {useEffect} from 'react';
-
+import React, { useEffect } from 'react';
+import { Typography } from '@mui/material';
+import RocketIcon from '@mui/icons-material/Rocket';
 
 const supabase = createClient('https://yutkfbluvhuoshqgbtxe.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl1dGtmYmx1dmh1b3NocWdidHhlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc2OTI5NDEsImV4cCI6MjAzMzI2ODk0MX0.eopTb_b240QiYOvDfsFa0UcXh4c4xHXS9NvU4TdOECc')
 
@@ -12,8 +13,8 @@ function Login() {
 
     useEffect(() => {
         supabase.auth.onAuthStateChange(async (event, session) => {
-        console.log('session :', session);
-    
+            console.log('session :', session);
+
             if (session?.user?.role === 'authenticated') {
                 navigate('/home')
             } else {
@@ -25,10 +26,12 @@ function Login() {
     return (
         <div className="App">
             <header className="App-header">
+                <Typography variant='h2'>Argus</Typography><RocketIcon sx={{ padding: 2 }} />
+                <Typography variant='body1' sx={{ paddingBottom: 3 }}>Your personal notebook for stock investments</Typography>
                 <Auth
                     supabaseClient={supabase}
                     providers={['google']}
-                    theme='dark'
+                    onlyThirdPartyProviders
                     appearance={{ theme: ThemeSupa }}
                 />
             </header>
